8822f3801aa2bf6bc60e88830dd719ada9ca2a27
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2024-12-20 11:42:00 +0700

    Finish set up connect Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import { alchemyApiKey, mnemonic } from "./secrets.json";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.28",  // Ensure this matches your contract's Solidity version
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${alchemyApiKey}`,
+      accounts: {
+        mnemonic: mnemonic,  // Using mnemonic to derive accounts
+      },
+    },
+  },
 };
 
 export default config;
